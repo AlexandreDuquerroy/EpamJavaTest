99e452711ab67b87a0b9b8de83faace7e597f7d4
AlexandreDuquerroy <43637634+AlexandreDuquerroy@users.noreply.github.com>
2018-09-27 13:34:50 +0200

    Adding format handling
    Checking format for png jpg and gif are OK 
    Check if file exist : ok 
    Check if format handled : OK
    Adding comments: OK

--- run.java
+++ run.java
@@ -12,58 +12,71 @@ import java.util.Scanner;
 /* Duquerroy Alexandre 
 	Epam Java Test Program
 	MagiczneNumerki.md
-	Update : 12:35 PM
+	Update : 1:33 PM
 */
 
 public class run {
 	
-	File f = new File("");
+	// Create arraylist to handle formats
 	static ArrayList fileformats = new ArrayList<String>()  {{
+		 add(".jpg");
+		 add(".jpg");
+		 add(".jpg");
 		 add(".png");
-		 add(".png");
-		 add(".png");
-		}};
-		
+		 add(".gif");
+		 add(".gif");
+		}};	
 	static ArrayList magicnumber = new ArrayList<String>()  {{
 		 add("ÿØÿÛ");
 		 add("ÿØÿà");
 		 add("ÿØÿá");
+		 add("‰PNG");
+		 add("GIF87a");
+		 add("GIF89a");
 		}};
-	public static String checkFormat(String buff){
+	// Method to check if the first line of the line analyzed contains signatures a.k.a magic numbers	
+	public static String checkMagicNumber(String buff){
 		String result = "";
-		for (int cpt0 = 0; cpt0<fileformats.size();cpt0++)
-			if(buff.contains(fileformats.get(cpt0).toString())){
-			result = ("The file is a " + magicnumber.get(cpt0).toString());
+		for (int cpt0 = 0; cpt0<magicnumber.size();cpt0++)
+			if(buff.contains(magicnumber.get(cpt0).toString())){
+				result = ("After analysis: the file is a " + fileformats.get(cpt0).toString());
 			}
-			else{System.out.println("FAIL");}
 		return result;
 	}
-
-	public static void main(String[] args) throws IOException {
+	// main 
+	public static void main(String[] args){
 		
 		String base_format="";
 		String src= "";
 		int dotindex = 0;
+		String firstfileline="";
+		//Creating scanner to get the path
 		Scanner in = new Scanner(System.in);
 		System.out.println("File Checking Program;");
 		System.out.println("Provide the absolute path to the file:");
 		src = in.nextLine();
 		dotindex= src.indexOf(".");
 		base_format =  src.substring(dotindex, src.length());
-		System.out.println("The file is a " + base_format);
-		try {
-			InputStream input = new FileInputStream(new File(src));
-			InputStreamReader lecture=new InputStreamReader(input);
-			BufferedReader buff=new BufferedReader(lecture);
-			String firstfileline="";
-			
-			firstfileline=buff.readLine();
-			System.out.println(firstfileline);
-			// Check if the extension is true
-			checkFormat(firstfileline);
-			buff.close(); 		
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+		// getting format
+		System.out.println("At first: The file is a " + base_format);
+		// check if format is handled
+		if(fileformats.contains(base_format)==false){
+			System.out.println("This kind of format is not handled by this software");
+		}
+		else{
+			try {
+				// Read the first line of the file
+				InputStream input = new FileInputStream(new File(src));
+				InputStreamReader lecture=new InputStreamReader(input);
+				BufferedReader buff=new BufferedReader(lecture);		
+				firstfileline=buff.readLine();
+				// Return the result of thecheckMagicNumber method
+				System.out.println(checkMagicNumber(firstfileline).toString());
+				buff.close(); 		
+			} catch (IOException e) {
+				System.out.println("This file cannot be found at specified path");
+				e.printStackTrace();
+			}
 		}
 	}
 }
