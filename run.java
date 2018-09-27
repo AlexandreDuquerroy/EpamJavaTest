1cf3a668e8672cb5468975e24b4e56ebf0307c29
AlexandreDuquerroy <43637634+AlexandreDuquerroy@users.noreply.github.com>
2018-09-27 11:55:47 +0200

    Strating checking path entry

---
+++ run.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.Scanner;
+/* Duquerroy Alexandre 
+	Epam Java Test Program
+	MagiczneNumerki.md
+	Starting : 11:46 AM
+*/
+
+public class run {
+	
+
+	public static void main(String[] args) {
+		String src= "";
+		Scanner in = new Scanner(System.in);
+		System.out.println("File Checking Program;");
+		System.out.println("Provide the absolute path to the file:");
+		src = in.nextLine();
+		
+		System.out.println(src);
+	}
+
+}
