269cd74aa67939a17f16989119a07212d2a61bba
AlexandreDuquerroy <43637634+AlexandreDuquerroy@users.noreply.github.com>
2018-09-27 12:26:54 +0200

    Reading a txt file : OK

--- run.java
+++ run.java
@@ -1,22 +1,43 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 import java.util.Scanner;
 /* Duquerroy Alexandre 
 	Epam Java Test Program
 	MagiczneNumerki.md
-	Starting : 11:46 AM
+	Update : 12:25 PM
 */
 
 public class run {
 	
+	File f = new File("");
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		
 		String src= "";
 		Scanner in = new Scanner(System.in);
 		System.out.println("File Checking Program;");
 		System.out.println("Provide the absolute path to the file:");
 		src = in.nextLine();
 		
-		System.out.println(src);
+		try {
+			InputStream input = new FileInputStream(new File(src));
+			InputStreamReader lecture=new InputStreamReader(input);
+			BufferedReader buff=new BufferedReader(lecture);
+			String firstfileline="";
+			
+			firstfileline=buff.readLine();
+			System.out.println(firstfileline);
+			buff.close(); 		
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		// Check if the extension is true
 	}
-
 }
